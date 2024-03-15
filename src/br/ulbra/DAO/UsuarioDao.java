dd535eb7e423601f911ba24e05f8673dca0e3a33
MacKathleen <kathleenmachado23@gmail.com>
2024-03-14 21:22:37 -0300

    Exemplo da aula de GitHub - Sistema login - primeiro comit by Kathleen Machado

---
+++ src/br/ulbra/DAO/UsuarioDao.java
@@ -0,0 +1,38 @@
+package br.ulbra.DAO;
+
+import com.mysql.jdbc.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class UsuarioDao {
+
+    Connection con;
+
+    public UsuarioDao() throws SQLException {
+        con = (Connection) ConnectionFactory.getConnection();
+    }
+
+    public boolean checkLogin(String login, String senha) {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        boolean check = false;
+
+        try {
+            stmt = con.prepareStatement("SELECT * FROM tbusuario2 WHERE emailUsu =  ? and  senhaUsu =  ? ");
+            stmt.setString(1, login);
+            stmt.setString(2, senha);
+            rs = stmt.executeQuery();
+            if (rs.next()) {
+                check = true;
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt, rs);
+        }
+        return check;
+
+    }
+}
